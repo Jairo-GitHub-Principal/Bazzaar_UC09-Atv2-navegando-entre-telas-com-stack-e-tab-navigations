59643b42cccc65e918cb4c02f1b18a5ee8f10c61
jairoSenac <jairocesa@outlook.com.br>
2022-10-19 22:32:08 -0300

    UC09 navegação entre telas stack e tab e icones nos botao de navegação

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Icones from 'react-native-vector-icons/Ionicons';
+import  Mi from 'react-native-vector-icons/MaterialIcons';
+
 
 // pagina  inicial
 
@@ -21,9 +24,13 @@ import StackSobre from './src/Paginas/PaginaInicial/SobreBazzaar';
 import StackCartao from './src/Paginas/cartoes/cartao';
 
 
-
+const iconHome = <Icones name="home-outline" size={30} color="black"/>
+const iconCategoria = <Mi name="category" size={30} color="black"/>
+const iconSobre = <Icones name="information-circle" size={30} color="black"/>
+const iconCard = <Icones name="card" size={30} color="black"/>
 
 function Cartoes(){
+  
 
 }
 
@@ -35,10 +42,10 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="categoria" component={CategoriaStack} />
-         <Tab.Screen name="Sobre" component={StackSobre} />
-        <Tab.Screen name="Cartões" component={StackCartao} />
+        <Tab.Screen name="Home" component={HomeStackScreen} options={{tabBarIcon: ({color,size}) => (iconHome),tabBarActiveTintColor: 'red',tabBarInactiveTintColor: 'black'}} />
+        <Tab.Screen  name="categoria" component={CategoriaStack} options={{tabBarIcon: ({color,size}) => (iconCategoria),tabBarActiveTintColor: 'red',tabBarInactiveTintColor: 'black'}}/>
+         <Tab.Screen name="Sobre" iconSobre component={StackSobre} options={{tabBarIcon: ({color,size}) => (iconSobre),tabBarActiveTintColor: 'red',tabBarInactiveTintColor: 'black'}}/> 
+        <Tab.Screen name="Cartões" component={StackCartao} options={ {tabBarIcon: ({color,size}) => (iconCard),tabBarActiveTintColor: 'red',tabBarInactiveTintColor: 'black'}}  />
 
       </Tab.Navigator>
     </NavigationContainer>
